db5a188dd368fc4b7f005b896aa8bc2e9083545b
artpimentel <art.ol.pimentel@gmail.com>
2025-10-22 15:27:51 -0300

    Lang Update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,25 @@
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
 import styles from "./Header.module.css";
+import { IoIosArrowDown, IoIosClose } from "react-icons/io";
 
-import { IoSearchOutline } from "react-icons/io5";
-
+import Popup from "@/components/ui/Popup/Popup";
 import LocationSelector from "@/components/LocationSelector/LocationSelector";
 import Dropdown from "../ui/Dropdown/Dropdown";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedLang, setSelectedLang] = useState("br");
+
   const languages = [
-    { code: "br", label: "Português" },
-    { code: "us", label: "English" },
-    { code: "es", label: "Español" },
+    { code: "br", country: "Brasil", label: "Português" },
+    { code: "us", country: "United States", label: "English" },
+    { code: "es", country: "España", label: "Español" },
   ];
+
+  const currentLang = languages.find((lang) => lang.code === selectedLang);
 
   return (
     <header className={styles.header}>
@@ -23,18 +29,6 @@ function Header() {
         </Link>
 
         <LocationSelector />
-
-        <div className={styles.langs}>
-          {languages.map((lang) => (
-            <i key={lang.code} className={styles.languageOption}>
-              <img
-                src={`/flags/${lang.code}.svg`}
-                alt={lang.label}
-                className={styles.flagProp}
-              />
-            </i>
-          ))}
-        </div>
 
         <div className={styles.headerButtons}>
           <Dropdown
@@ -49,9 +43,56 @@ function Header() {
               Como Anunciante
             </Link>
           </Dropdown>
+
           <Link href="/signin" className={styles.logInTrigger}>
             Entre
           </Link>
+
+          <Popup
+            trigger={
+              <>
+                <img
+                  src={`/flags/${currentLang?.code}.svg`}
+                  alt={currentLang?.label}
+                  className={styles.flagProp}
+                />
+                <IoIosArrowDown />
+              </>
+            }
+            triggerClass={styles.trigger}
+            popupClass={styles.popup}
+            isOpen={isOpen}
+            onOpenChange={setIsOpen}
+          >
+            <div className={styles.popupHeader}>
+              <span>Região e Linguagem</span>{" "}
+              <button className={styles.closeButton}>
+                <IoIosClose />
+              </button>
+            </div>
+            {languages.map((lang) => (
+              <button
+                key={lang.code}
+                className={`${styles.languageOption} ${
+                  lang.code === selectedLang ? styles.activeLang : ""
+                }`}
+                onClick={() => {
+                  setSelectedLang(lang.code);
+                  setIsOpen(false);
+                }}
+              >
+                <img
+                  src={`/flags/${lang.code}.svg`}
+                  alt={lang.label}
+                  className={styles.flagProp}
+                />
+                <div className={styles.langLabel}>
+                  <strong>{lang.country}</strong>
+                  {lang.label}
+                </div>
+              </button>
+            ))}
+          </Popup>
         </div>
       </div>
     </header>
